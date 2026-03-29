293405b79dba7b1a588d1d82307acc93c8c2d810
Felix <KLDXYO@outlook.com>
2026-03-29 21:48:01 +0800

    build: Update example

--- src/module/example.py
+++ src/module/example.py
@@ -2,9 +2,13 @@ import slim
 
 
 class Test:
-    def enter(self, name: str):
-        print("Hello " + str(name))
+    def enter(self, name: str | None):
+        if name:
+            print("Hello " + name)
+        else:
+            name = input("So,what are you name?\nInput your name:")
+            print(f"Hello {name}!Nice to meet you.")
 
 
-slim.register("hello", "[name=Felix]", Test())
+slim.register("hello", "[name]", Test())
 slim.run()
